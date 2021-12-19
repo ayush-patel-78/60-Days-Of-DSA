89ab20e71abedb8f717274917213174decae8e0b
Ayush <ayushkurmi97@gmail.com>
2021-12-19 23:07:07 +0530

    friendmeetup

---
+++ Day 11/friendmeetup.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	long long int t,x1,x2;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>x1>>x2;
+	    if(x1>=x2){
+	        cout<<"yes"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	    
+	}
+	
+	return 0;
+}
