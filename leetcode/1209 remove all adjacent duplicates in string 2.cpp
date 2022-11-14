4d89885b91370b38f25082be14f79d1b0ef7a574
ayush-patel-78 <ayushkurmi97@gmail.com>
2022-11-15 00:49:42 +0530

    most stones removed with same row or col

--- leetcode/1209 remove all adjacent duplicates in string 2.cpp
+++ leetcode/1209 remove all adjacent duplicates in string 2.cpp
@@ -1,34 +1,82 @@
+class DisjointSet {
+    vector<int> rank, parent, size; 
+public: 
+    DisjointSet(int n) {
+        rank.resize(n+1, 0); 
+        parent.resize(n+1);
+        size.resize(n+1); 
+        for(int i = 0;i<=n;i++) {
+            parent[i] = i; 
+            size[i] = 1; 
+        }
+    }
+
+    int findUPar(int node) {
+        if(node == parent[node])
+            return node; 
+        return parent[node] = findUPar(parent[node]); 
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u); 
+        int ulp_v = findUPar(v); 
+        if(ulp_u == ulp_v) return; 
+        if(rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v; 
+        }
+        else if(rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u; 
+        }
+        else {
+            parent[ulp_v] = ulp_u; 
+            rank[ulp_u]++; 
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u); 
+        int ulp_v = findUPar(v); 
+        if(ulp_u == ulp_v) return; 
+        if(size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v; 
+            size[ulp_v] += size[ulp_u]; 
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v]; 
+        }
+    }
+}; 
 class Solution {
 public:
-    string removeDuplicates(string s, int k){
-        string result;
-        stack<pair<char,int>> st;
-        for(int i = 0;i < s.size(); i++){
-            if(st.empty()){
-                st.push({s[i],1});
-                continue;
-            }
-            if(st.top().first == s[i] ){
-                if(st.top().second == k-1){
-                    for(int j = 0; j < k-1; j++){
-                        st.pop();
-                    }
-                }
-                else{
-                    st.push({s[i],st.top().second + 1});
-                }
-            }
-            else{
-                st.push({s[i],1});
-            }
-        }
+    int removeStones(vector<vector<int>>& stones) {
+        int maxRow = 0;
+        int maxCol = 0;
 
-        string ans ="";
-        while(!st.empty()){
-            ans.push_back(st.top().first);
-            st.pop();
+        for(auto it: stones){
+            maxRow = max(maxRow,it[0]);
+            maxCol = max(maxCol,it[1]);
         }
-        reverse(ans.begin(), ans.end());
-        return ans;
+        DisjointSet ds(maxRow + maxCol + 2);
+        unordered_map<int,int> vis;
+        // col = col + maxRow + 1
+
+       
+        for(auto it: stones){
+            int nodeRow = it[0];
+            int nodeCol = it[1]+maxRow +1;
+            ds.unionBySize(nodeRow,nodeCol);
+            vis[nodeRow]=1;
+            vis[nodeCol] = 1;
+        }
+        int components = 0;
+        for(auto it:vis){
+          
+            if(ds.findUPar(it.first)==it.first) components++;
+        }
+        
+        return stones.size() - components;
     }
 };
+
+
